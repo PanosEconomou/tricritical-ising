f9bcd7f383aa2ddb83e5d5fcc59c979df181801f
Panos Oikonomou <po524@nyu.edu>
2025-03-26 18:31:27 -0400

    colors

--- lie-calculations/conformal-weights.py
+++ lie-calculations/conformal-weights.py
@@ -3,6 +3,12 @@ from fractions import Fraction
 
 np.set_printoptions(formatter={'all': lambda x: str(Fraction(x).limit_denominator())})
 
+BLD = '\033[1m'
+RST = '\033[0m'
+BRD = '\033[91m'
+BCY = '\033[96m'
+BGR = '\033[32m'
+
 k   = 8;
 L   = np.arange(k+1)/2
 LM  = np.array([(l,m/2) for l in L for m in range(-int(2*l),int(2*l)+1,2)])
@@ -21,9 +27,10 @@ LM = LM[idx]
 H   = lambda l,m,k: l*(l+1)/(k+2) - m**2/k
 h   = np.array([H(*lm,k) for lm in LM])
 
-print("Coset: ",np.sort(h),len(h))
-
 hi = np.array([ i+j for i in [0,7/16,1/10,3/5,3/80,3/2] for j in [0,7/16,1/10,3/5,3/80,3/2]])
 
-print("\nIsing: ",np.sort(hi), len(hi))
+
+print(BLD+BGR+"Here are the conformal weights"+RST)
+print(BLD+BCY+"\nCoset: "+RST,np.sort(h),len(h))
+print(BLD+BRD+"\nIsing: "+RST,np.sort(hi), len(hi))
 
